55444fdde98556cafa2d8cfcad4a6fd8bed220de
jhonny <ismaelherz2001@gmail.com>
2021-10-22 01:09:42 -0600

    Configuracion de ancho de banda (subida-bajada)

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 #Variables
 INTERFACE_IN = 'enp7s0'
 INTERFACE_OUT = 'enp8s0'
-IP = '10.10.10.2'
+IP = '10.10.10.3'
 
 #reglas de filtros a aplicar para ancho de banda de subida como de bajada
 IN=f"tc filter add dev {INTERFACE_IN} parent 1:0 protocol ip prio 1 u32 match ip dst "
@@ -15,20 +15,20 @@ IFB = "modprobe ifb numifbs=1"
 
 #hablitar interfaz para upload
 C1 = f"ip link set dev {INTERFACE_OUT} up"
-C2 = f"tc qdisc del dev {INTERFACE_IN}root 2>/dev/null"
-C3 = f"tc qdisc del dev {INTERFACE_IN}ingress 2>/dev/null"
+C2 = f"tc qdisc del dev {INTERFACE_IN} root 2>/dev/null"
+C3 = f"tc qdisc del dev {INTERFACE_IN} ingress 2>/dev/null"
 C4 = f"tc qdisc del dev {INTERFACE_OUT} root 2>/dev/null"
-C5 = f"tc qdisc add dev {INTERFACE_IN}handle ffff: ingress"
-C6 = f"""tc filter add dev {INTERFACE_IN}parent ffff: protocol ip u32 match u32 0 0 
-action mirred egress redirect dev $INTERFACE_OUT"""
+C5 = f"tc qdisc add dev {INTERFACE_IN} handle ffff: ingress"
+C6 = f"""tc filter add dev {INTERFACE_IN} parent ffff: protocol ip u32 match u32 0 0 
+action mirred egress redirect dev {INTERFACE_OUT}"""
 
 #creando enlance para bajada
-COMANDO_IN_ROOT = f"tc qdisc add dev {INTERFACE_IN}root handle 1: htb"
-COMANDO_IN_ENLACE = f"tc class add dev {INTERFACE_IN}parent 1: classid 1:11 htb rate 1mbit"
+COMANDO_IN_ROOT = f"tc qdisc add dev {INTERFACE_IN} root handle 1: htb"
+COMANDO_IN_ENLACE = f"tc class add dev {INTERFACE_IN} parent 1: classid 1:11 htb rate 1mbit"
 
 #Creando enlace para subida
 COMANDO_OUT_ROOT = f"tc qdisc add dev {INTERFACE_OUT} root handle 1: htb"
-COMANDO_OUT_ENLACE = f"tc class add dev {INTERFACE_OUT} parent 1: classid 1:11 htb rate 1024kbit"
+COMANDO_OUT_ENLACE = f"tc class add dev {INTERFACE_OUT} parent 1: classid 1:11 htb rate 512kbit"
 
 
 #asignano ip a enlace
--- configurar_isp.py
+++ configurar_isp.py
@@ -1,11 +1,28 @@
+from os import system
 from . import commands
 
 #funciones
 def ejecutar_command(comando):
-    pass
+    try:
+        system(comando)
+    except:
+        print("Error")
+
 #configurar isp
 ISP_UP = f"ip addr add 10.10.10.1/24 dev {commands.INTERFACE_IN}"
 ISP_DOWN = f"ip addr add 10.10.10.2/24 dev {commands.INTERFACE_OUT}"
 
 ejecutar_command(ISP_UP)
 ejecutar_command(ISP_DOWN)
+ejecutar_command(commands.C1)
+ejecutar_command(commands.C2)
+ejecutar_command(commands.C3)
+ejecutar_command(commands.C4)
+ejecutar_command(commands.C5)
+ejecutar_command(commands.C6)
+ejecutar_command(commands.COMANDO_IN_ROOT)
+ejecutar_command(commands.COMANDO_IN_ENLACE)
+ejecutar_command(commands.COMANDO_OUT_ROOT)
+ejecutar_command(commands.COMANDO_OUT_ENLACE)
+ejecutar_command(commands.DOWNLOAD)
+ejecutar_command(commands.UPLOAD)
