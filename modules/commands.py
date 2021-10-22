3536036bd9aa2e83a443da0a25de983d79e53a92
jhonny <ismaelherz2001@gmail.com>
2021-10-22 11:22:15 -0600

    .

--- modules/commands.py
+++ modules/commands.py
@@ -4,14 +4,14 @@ INTERFACE_OUT = 'enp8s0'
 IP = '10.10.10.3'
 
 #reglas de filtros a aplicar para ancho de banda de subida como de bajada
-IN=f"/usr/sbin/tc  filter add dev {INTERFACE_IN} parent 1:0 protocol ip prio 1 u32 match ip dst "
-OUT=f"/usr/sbin/tc  filter add dev {INTERFACE_OUT} parent 1:0 protocol ip prio 1 u32 match ip src "
+IN=f"/usr/sbin/tc  filter add dev {INTERFACE_IN} parent 1:0 protocol ip prio 1 u32 match ip dst"
+OUT=f"/usr/sbin/tc  filter add dev {INTERFACE_OUT} parent 1:0 protocol ip prio 1 u32 match ip src"
 PUERTO="match ip sport "
 REGLA="0xffff flowid"
 
 #insertar modulo ifb, asignando el numero de interfacez virtuales 
 # que se necesita por defecto es 2
-IFB = "sudo modprobe ifb numifbs=1"
+IFB = "sudo modprobe ifb numifbs=2"
 
 #hablitar interfaz para upload
 C1 = f"ip link set dev {INTERFACE_OUT} up"
@@ -23,12 +23,13 @@ C6 = f"""/usr/sbin/tc  filter add dev {INTERFACE_IN} parent ffff: protocol ip u3
 
 #creando enlance para bajada
 COMANDO_IN_ROOT = f"/usr/sbin/tc  qdisc add dev {INTERFACE_IN} root handle 1: htb"
-COMANDO_IN_ENLACE = f"/usr/sbin/tc  class add dev {INTERFACE_IN} parent 1: classid 1:11 htb rate 1mbit"
+COMANDO_IN_ENLACE = f"/usr/sbin/tc  class add dev {INTERFACE_IN} parent 1: classid 1:10 htb rate 1000kbit"
+COMANDO_EXTRA_IN = f"tc qdisc add dev {INTERFACE_IN} parent 1:10 handle 10: sfq perturb 10"
 
 #Creando enlace para subida
 COMANDO_OUT_ROOT = f"/usr/sbin/tc  qdisc add dev {INTERFACE_OUT} root handle 1: htb"
-COMANDO_OUT_ENLACE = f"/usr/sbin/tc  class add dev {INTERFACE_OUT} parent 1: classid 1:11 htb rate 512kbit"
-
+COMANDO_OUT_ENLACE = f"/usr/sbin/tc  class add dev {INTERFACE_OUT} parent 1: classid 1:10 htb rate 512kbit"
+COMANDO_EXTRA_OUT = f"tc qdisc add dev {INTERFACE_OUT} parent 1:10 handle 10: sfq perturb 10"
 
 #asignano ip a enlace
 DOWNLOAD = f"{IN} {IP} flowid 1:11"
