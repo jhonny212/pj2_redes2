1a0ea639fd54f214a555b4c74f00c0f847672e3c
jhonny <ismaelherz2001@gmail.com>
2021-10-24 13:29:16 -0600

    .

--- balanceador_carga.py
+++ balanceador_carga.py
@@ -1,10 +1,28 @@
-from modules import commands_balanceador
+from modules import commands_balanceador as commands
+from os import system
 
 #Definir tablas de rutas y prioridad
-rt_tables = open(commands_balanceador.PATH_ROUTES)
+rt_tables = open(commands.PATH_ROUTES)
 content = rt_tables.read()
 content += "2   T0\n"
 content += "3   T1"
+rt_tables.write(content)
+rt_tables.close()
 
+#funciones
+def ejecutar_command(comando):
+    try:
+        print("Ejecutando... ",comando)
+        system(comando)
+        print("\n")
+    except:
+        print("Error")
 #crear enrutamiento
 
+ejecutar_command(commands.R1)
+ejecutar_command(commands.R2)
+ejecutar_command(commands.R3)
+ejecutar_command(commands.R4)
+ejecutar_command(commands.R5)
+ejecutar_command(commands.R6)
+ejecutar_command(commands.P1)
