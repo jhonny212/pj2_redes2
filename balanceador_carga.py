9ca172482fb7a935c981a64d02537b8345575c97
jhonny <ismaelherz2001@gmail.com>
2021-10-29 20:09:18 -0600

    .2

--- balanceador_carga.py
+++ balanceador_carga.py
@@ -6,7 +6,7 @@ rt_tables = open(commands.PATH_ROUTES)
 content = rt_tables.read()
 content += "2   T0\n"
 content += "3   T1"
-rt_tables.write(content)
+#rt_tables.write(content)
 rt_tables.close()
 
 #funciones
@@ -18,6 +18,13 @@ def ejecutar_command(comando):
     except:
         print("Error")
 #crear enrutamiento
+opc = 1
+if opc==1:
+    ejecutar_command("ip add del 10.10.10.3/24 dev enp1s0")
+    ejecutar_command("ip add add 10.10.10.3/24 dev enp1s0")
+    ejecutar_command("ip add del 20.20.20.3/24 dev enp7s0")
+    ejecutar_command("ip add add 20.20.20.3/24 dev enp7s0")
+
 
 ejecutar_command(commands.R1)
 ejecutar_command(commands.R2)
@@ -25,4 +32,7 @@ ejecutar_command(commands.R3)
 ejecutar_command(commands.R4)
 ejecutar_command(commands.R5)
 ejecutar_command(commands.R6)
-ejecutar_command(commands.P1)
+ejecutar_command(commands.R7)
+ejecutar_command(commands.R8)
+#ejecutar_command(commands.P1)
+print(commands.P1)
--- modules/commands_balanceador.py
+++ modules/commands_balanceador.py
@@ -15,21 +15,24 @@ GTW1 = '10.10.10.1'
 GTW2 = '20.20.20.1'
 
 #Subred
-SUBRE1 = '10.10.10.0'
-SUBRE2 = '20.20.20.0'
+SUBRE1 = '10.10.10.0/24'
+SUBRE2 = '20.20.20.0/24'
 
 PATH_ROUTES = '/etc/iproute2/rt_tables'
 
 #Rutas de encaminamiento
-R1 = f'ip route add default dev {INTERFAZ_DOWNLOAD_ISP1} via {GTW1} dev table T0'
-R2 = f'ip route add {SUBRE1} dev {INTERFAZ_DOWNLOAD_ISP1} src {IP_DOWNLOAD_ISP1} table T0'
+R2 = f'ip route add default dev {INTERFAZ_DOWNLOAD_ISP1} via {GTW1} table T0'
+R1 = f'ip route add {SUBRE1} dev {INTERFAZ_DOWNLOAD_ISP1} src {IP_DOWNLOAD_ISP1} table T0'
 
-R3 = f'ip route add default dev {INTERFAZ_DOWNLOAD_ISP2} via {GTW2} dev table T1'
-R4 = f'ip route add {SUBRE2} dev {INTERFAZ_DOWNLOAD_ISP2} src {IP_DOWNLOAD_ISP2} table T1'
+R4 = f'ip route add default dev {INTERFAZ_DOWNLOAD_ISP2} via {GTW2} table T1'
+R3 = f'ip route add {SUBRE2} dev {INTERFAZ_DOWNLOAD_ISP2} src {IP_DOWNLOAD_ISP2} table T1'
 
 #Reglas de encaminamiento
 R5 = f'ip rule add from {IP_DOWNLOAD_ISP1}/32 table T0'
-R6 = f'ip rule add from {IP_DOWNLOAD_ISP2}/32 table T1'
+R6 = f'ip rule add to {IP_DOWNLOAD_ISP1}/32 table T0'
+
+R7 = f'ip rule add from {IP_DOWNLOAD_ISP2}/32 table T1'
+R8 = f'ip rule add to {IP_DOWNLOAD_ISP2}/32 table T1'
 
 #Configuracion de pesos
 P1 = f'ip route add default scope global nexthop via '
